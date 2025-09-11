996bdfe9c9e6b45333dc69513d1711156b8c3258
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2025-09-11 18:33:23 +0600

    Create ReverseTriangle.cpp

---
+++ ReverseTriangle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  main()
+{
+    int x;
+    cin >> x;
+    for(int i=0;i<x;++i)
+    {
+        for(int j=i;j<x;++j)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
